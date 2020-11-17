6cd6514816ca6da670ba2837550f99677d1c31ed
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-11-17 07:43:55 +0700

    Update SplitMix64.cs
    simplify

--- Security/RNG/PRNG/SplitMix64.cs
+++ Security/RNG/PRNG/SplitMix64.cs
@@ -45,7 +45,7 @@ namespace Litdex.Security.RNG.PRNG
 		/// <inheritdoc/>
 		protected override ulong Next()
 		{
-			this._Seed = this._Seed + 0x9E3779B97F4A7C15UL;
+			this._Seed += 0x9E3779B97F4A7C15UL;
 			var result = this._Seed;
 			result = (result ^ (result >> 30)) * 0xBF58476D1CE4E5B9UL;
 			result = (result ^ (result >> 27)) * 0x94D049BB133111EBUL;
