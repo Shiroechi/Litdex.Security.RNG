4da5578c81ad1126c1a98d72da226aaccaf70055
Restyled.io <commits@restyled.io>
2020-11-17 06:29:21 +0000

    Restyled by clang-format

--- Security/RNG/Random32.cs
+++ Security/RNG/Random32.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 
-namespace Litdex.Security.RNG
-{
-/// <summary>
-/// Base class for 32 bit RNG.
-/// </summary>
-public abstract class Random32 : Random
-{
-    #region Protected Method
+namespace Litdex.Security.RNG {
+  /// <summary>
+  /// Base class for 32 bit RNG.
+  /// </summary>
+  public abstract class Random32 : Random {
+#region Protected Method
 
     /// <summary>
     /// Generate next random number.
@@ -15,57 +13,44 @@ public abstract class Random32 : Random
     /// <returns>A 32-bit unsigned integer.</returns>
     protected abstract uint Next();
 
-    #endregion Protected Method
+#endregion Protected Method
 
-    #region Public Method
-
-    /// <inheritdoc/>
-    public override string AlgorithmName()
-    {
-        return "Random32";
-    }
+#region Public Method
 
     /// <inheritdoc/>
-    public override bool NextBoolean()
-    {
-        return this.NextInt() >> 31 == 0;
-    }
+    public override string AlgorithmName() { return "Random32"; }
 
     /// <inheritdoc/>
-    public override byte[] NextBytes(int length)
-    {
-        if (length <= 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(length), $"The requested output size can't lower than 1.");
+    public override bool NextBoolean() { return this.NextInt() >> 31 == 0; }
+
+    /// <inheritdoc/>
+    public override byte[] NextBytes(int length) {
+      if (length <= 0) {
+        throw new ArgumentOutOfRangeException(
+            nameof(length), $"The requested output size can't lower than 1.");
+      }
+
+      uint sample = 0;
+      var data = new byte[length];
+
+      for (var i = 0; i < length; i++) {
+        if (i % 4 == 0) {
+          sample = this.Next();
         }
-
-        uint sample = 0;
-        var data = new byte[length];
-
-        for (var i = 0; i < length; i++)
-        {
-            if (i % 4 == 0)
-            {
-                sample = this.Next();
-            }
-            data[i] = (byte)sample;
-            sample >>= 8;
-        }
-        return data;
+        data[i] = (byte) sample;
+        sample >>= 8;
+      }
+      return data;
     }
 
     /// <inheritdoc/>
-    public override uint NextInt()
-    {
-        return this.Next();
-    }
+    public override uint NextInt() { return this.Next(); }
 
     /// <inheritdoc/>
-    public override ulong NextLong()
-    {
-        return BitConverter.ToUInt64(this.NextBytes(8), 0);
+    public override ulong NextLong() {
+      return BitConverter.ToUInt64(this.NextBytes(8), 0);
     }
 
-    #endregion Public Method
-}
+#endregion Public Method
+  }
 }
--- Security/RNG/Random64.cs
+++ Security/RNG/Random64.cs
@@ -1,13 +1,11 @@
 ﻿using System;
 
-namespace Litdex.Security.RNG
-{
-/// <summary>
-/// Base class for 64 bit RNG.
-/// </summary>
-public abstract class Random64 : Random
-{
-    #region Protected Method
+namespace Litdex.Security.RNG {
+  /// <summary>
+  /// Base class for 64 bit RNG.
+  /// </summary>
+  public abstract class Random64 : Random {
+#region Protected Method
 
     /// <summary>
     /// Generate next random number.
@@ -15,57 +13,42 @@ public abstract class Random64 : Random
     /// <returns>A 64-bit unsigned integer.</returns>
     protected abstract ulong Next();
 
-    #endregion Protected Method
+#endregion Protected Method
 
-    #region Public Method
-
-    /// <inheritdoc/>
-    public override string AlgorithmName()
-    {
-        return "Random64";
-    }
+#region Public Method
 
     /// <inheritdoc/>
-    public override bool NextBoolean()
-    {
-        return this.Next() >> 63 == 0;
-    }
+    public override string AlgorithmName() { return "Random64"; }
 
     /// <inheritdoc/>
-    public override byte[] NextBytes(int length)
-    {
-        if (length <= 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(length), $"The requested output size can't lower than 1.");
+    public override bool NextBoolean() { return this.Next() >> 63 == 0; }
+
+    /// <inheritdoc/>
+    public override byte[] NextBytes(int length) {
+      if (length <= 0) {
+        throw new ArgumentOutOfRangeException(
+            nameof(length), $"The requested output size can't lower than 1.");
+      }
+
+      ulong sample = 0;
+      var data = new byte[length];
+
+      for (var i = 0; i < length; i++) {
+        if (i % 8 == 0) {
+          sample = this.Next();
         }
-
-        ulong sample = 0;
-        var data = new byte[length];
-
-        for (var i = 0; i < length; i++)
-        {
-            if (i % 8 == 0)
-            {
-                sample = this.Next();
-            }
-            data[i] = (byte)sample;
-            sample >>= 8;
-        }
-        return data;
+        data[i] = (byte) sample;
+        sample >>= 8;
+      }
+      return data;
     }
 
     /// <inheritdoc/>
-    public override uint NextInt()
-    {
-        return (uint)this.Next();
-    }
+    public override uint NextInt() { return (uint) this.Next(); }
 
     /// <inheritdoc/>
-    public override ulong NextLong()
-    {
-        return this.Next();
-    }
+    public override ulong NextLong() { return this.Next(); }
 
-    #endregion Public Method
-}
+#endregion Public Method
+  }
 }
