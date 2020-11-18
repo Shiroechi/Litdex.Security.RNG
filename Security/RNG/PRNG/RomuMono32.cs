bb7c2c4429ee5500e186f8f85c413822a7a4ea64
Restyled.io <commits@restyled.io>
2020-11-18 11:58:55 +0000

    Restyled by astyle

--- Security/RNG/PRNG/RomuMono32.cs
+++ Security/RNG/PRNG/RomuMono32.cs
@@ -3,70 +3,70 @@ using System.Security.Cryptography;
 
 namespace Litdex.Security.RNG
 {
-	/// <summary>
-	/// 32-bit arithmetic: Suitable only up to 2^26 output-values. Outputs 16-bit numbers.
-	/// Fixed period of (2^32)-47. Must be seeded using the romuMono32_init function.
-	/// Capacity = 2^27 bytes. Register pressure = 2. State size = 32 bits.
-	/// </summary>
-	public class RomuMono32 : Random32
-	{
-		#region Member
+/// <summary>
+/// 32-bit arithmetic: Suitable only up to 2^26 output-values. Outputs 16-bit numbers.
+/// Fixed period of (2^32)-47. Must be seeded using the romuMono32_init function.
+/// Capacity = 2^27 bytes. Register pressure = 2. State size = 32 bits.
+/// </summary>
+public class RomuMono32 : Random32
+{
+    #region Member
 
-		private uint _Seed;
+    private uint _Seed;
 
-		#endregion Member
+    #endregion Member
 
-		#region Constructor & Destructor
+    #region Constructor & Destructor
 
-		public RomuMono32(uint seed = 0)
-		{
-			this._Seed = (seed & 0x1fffffffu) + 1156979152u;  // Accepts 29 seed-bits.;
-		}
+    public RomuMono32(uint seed = 0)
+    {
+        this._Seed = (seed & 0x1fffffffu) + 1156979152u;  // Accepts 29 seed-bits.;
+    }
 
-		~RomuMono32()
-		{
-			this._Seed = 0;
-		}
+    ~RomuMono32()
+    {
+        this._Seed = 0;
+    }
 
-		#endregion Constructor & Destructor
+    #endregion Constructor & Destructor
 
-		#region Protected Method
+    #region Protected Method
 
-		/// <inheritdoc/>
-		protected override uint Next()
-		{
-			uint result = this._Seed >> 16;
-			this._Seed *= 3611795771u;
-			this._Seed = this.ROTL(this._Seed, 12);
-			return result;
-		}
+    /// <inheritdoc/>
+    protected override uint Next()
+    {
+        uint result = this._Seed >> 16;
+        this._Seed *= 3611795771u;
+        this._Seed = this.ROTL(this._Seed, 12);
+        return result;
+    }
 
-		protected uint ROTL(uint d, int lrot)
-		{
-			return (d << (lrot)) | (d >> (32 - lrot));
-		}
+    protected uint ROTL(uint d, int lrot)
+    {
+        return (d << (lrot)) | (d >> (32 - lrot));
+    }
 
-		#endregion Protected Method
+    #endregion Protected Method
 
-		#region Public Method
+    #region Public Method
 
-		/// <inheritdoc/>
-		public override string AlgorithmName()
-		{
-			return "Romu Mono 32 bit";
-		}
+    /// <inheritdoc/>
+    public override string AlgorithmName()
+    {
+        return "Romu Mono 32 bit";
+    }
 
-		/// <inheritdoc/>
-		public override void Reseed()
-		{
-			using (var rng = new RNGCryptoServiceProvider())
-			{
-				var bytes = new byte[4];
-				rng.GetNonZeroBytes(bytes);
-				this._Seed = BitConverter.ToUInt32(bytes, 0);
-			}
-		}
+    /// <inheritdoc/>
+    public override void Reseed()
+    {
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            var bytes = new byte[4];
+            rng.GetNonZeroBytes(bytes);
+            this._Seed = BitConverter.ToUInt32(bytes, 0);
+        }
+    }
 
-		#endregion Public Method
-	}
+    #endregion Public Method
+}
 }
--- Security/RNG/PRNG/Tyche.cs
+++ Security/RNG/PRNG/Tyche.cs
@@ -5,105 +5,105 @@ using System.Security.Cryptography;
 
 namespace Litdex.Security.RNG.PRNG
 {
-	/// <summary>
-	/// <see cref="Tyche"/> is based on ChaCha's quarter-round.
-	/// </summary>
-	public class Tyche : Random32
-	{
-		#region Member
+/// <summary>
+/// <see cref="Tyche"/> is based on ChaCha's quarter-round.
+/// </summary>
+public class Tyche : Random32
+{
+    #region Member
 
-		private uint _A, _B, _C, _D;
+    private uint _A, _B, _C, _D;
 
-		#endregion Member
+    #endregion Member
 
-		#region Constructor & Destructor
+    #region Constructor & Destructor
 
-		public Tyche(ulong seed = 0, uint idx = 0)
-		{
-			this.Init(seed, idx);
-			for (var i = 0; i < 20; i++)
-			{
-				this.Mix();
-			}
-		}
+    public Tyche(ulong seed = 0, uint idx = 0)
+    {
+        this.Init(seed, idx);
+        for (var i = 0; i < 20; i++)
+        {
+            this.Mix();
+        }
+    }
 
-		~Tyche()
-		{
-			this._A = this._B = this._C = this._D = 0;
-		}
+    ~Tyche()
+    {
+        this._A = this._B = this._C = this._D = 0;
+    }
 
-		#endregion Constructor & Destructor
+    #endregion Constructor & Destructor
 
-		#region Protected Method
+    #region Protected Method
 
-		/// <inheritdoc/>
-		protected override uint Next()
-		{
-			this.Mix();
-			return this._B;
-		}
+    /// <inheritdoc/>
+    protected override uint Next()
+    {
+        this.Mix();
+        return this._B;
+    }
 
-		/// <summary>
-		/// Initialzied internal state.
-		/// </summary>
-		/// <param name="seed"></param>
-		/// <param name="idx"></param>
-		protected void Init(ulong seed, uint idx)
-		{
-			this._A = (uint)(seed / uint.MaxValue);
-			this._B = (uint)(seed % uint.MaxValue);
-			this._C = 2654435769;
-			this._D = idx ^ 1367130551;
-		}
+    /// <summary>
+    /// Initialzied internal state.
+    /// </summary>
+    /// <param name="seed"></param>
+    /// <param name="idx"></param>
+    protected void Init(ulong seed, uint idx)
+    {
+        this._A = (uint)(seed / uint.MaxValue);
+        this._B = (uint)(seed % uint.MaxValue);
+        this._C = 2654435769;
+        this._D = idx ^ 1367130551;
+    }
 
-		/// <summary>
-		/// Update internal state based on quater round function of ChaCha stream chiper.
-		/// </summary>
-		protected void Mix()
-		{
-			this._A += this._B;
-			this._D ^= this._A;
-			this._D = this._D << 16 | this._D >> 16;
+    /// <summary>
+    /// Update internal state based on quater round function of ChaCha stream chiper.
+    /// </summary>
+    protected void Mix()
+    {
+        this._A += this._B;
+        this._D ^= this._A;
+        this._D = this._D << 16 | this._D >> 16;
 
-			this._C += this._D;
-			this._B ^= this._C;
-			this._B = this._B << 12 | this._B >> 20;
+        this._C += this._D;
+        this._B ^= this._C;
+        this._B = this._B << 12 | this._B >> 20;
 
-			this._A += this._B ;
-			this._D ^= this._A;
-			this._D = this._D << 8 | this._D >> 24;
+        this._A += this._B ;
+        this._D ^= this._A;
+        this._D = this._D << 8 | this._D >> 24;
 
-			this._C += this._D ;
-			this._B ^= this._C;
-			this._B = this._B << 7 | this._B >> 25;
-		}
+        this._C += this._D ;
+        this._B ^= this._C;
+        this._B = this._B << 7 | this._B >> 25;
+    }
 
-		#endregion Protected Method
+    #endregion Protected Method
 
-		#region Public Method
+    #region Public Method
 
-		/// <inheritdoc/>
-		public override string AlgorithmName()
-		{
-			return "Tyche";
-		}
+    /// <inheritdoc/>
+    public override string AlgorithmName()
+    {
+        return "Tyche";
+    }
 
-		/// <inheritdoc/>
-		public override void Reseed()
-		{
-			using (var rng = new RNGCryptoServiceProvider())
-			{
-				var bytes = new byte[8];
-				rng.GetNonZeroBytes(bytes);
-				this.Init(BitConverter.ToUInt32(bytes, 0), 0);
-			}
+    /// <inheritdoc/>
+    public override void Reseed()
+    {
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            var bytes = new byte[8];
+            rng.GetNonZeroBytes(bytes);
+            this.Init(BitConverter.ToUInt32(bytes, 0), 0);
+        }
 
-			for (var i = 0; i < 20; i++)
-			{
-				this.Mix();
-			}
-		}
+        for (var i = 0; i < 20; i++)
+        {
+            this.Mix();
+        }
+    }
 
-		#endregion Public Method
-	}
+    #endregion Public Method
+}
 }
