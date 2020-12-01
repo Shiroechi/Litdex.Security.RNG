6240ddf65136d14bc955ab56cb0d911745db48ea
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2020-12-01 11:06:50 +0700

    Random32: improve perfomance generating byte array

--- Security/RNG/Random32.cs
+++ Security/RNG/Random32.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+
+#if NET5_0
+using System.Buffers.Binary;
+#endif
 
 namespace Litdex.Security.RNG
 {
@@ -39,19 +44,56 @@ namespace Litdex.Security.RNG
 				throw new ArgumentOutOfRangeException(nameof(length), $"The requested output size can't lower than 1.");
 			}
 
-			uint sample = 0;
-			var data = new byte[length];
+#if NET5_0
+			var chunk = new Span<byte>(new byte[4]);
+			List<byte> output = new List<byte>(length);
 
-			for (var i = 0; i < length; i++)
+			while (length >= 4)
 			{
-				if (i % 4 == 0)
-				{
-					sample = this.Next();
-				}
-				data[i] = (byte)sample;
-				sample >>= 8;
+				BinaryPrimitives.WriteUInt32LittleEndian(chunk, this.Next());
+				output.AddRange(chunk.ToArray());
+				length -= 4;
 			}
-			return data;
+
+			if (data != 0)
+			{
+				BinaryPrimitives.WriteUInt32LittleEndian(chunk, this.Next());
+				output.AddRange(chunk.Slice(0, length).ToArray());
+			}
+
+			return output.ToArray();
+#else
+			uint sample = 0;
+			var chunk = new byte[4];
+			List<byte> output = new List<byte>(length);
+
+			while (length >= 4)
+			{
+				sample = this.Next();
+
+				chunk[0] = (byte)sample;
+				chunk[1] = (byte)(sample >> 8);
+				chunk[2] = (byte)(sample >> 16);
+				chunk[3] = (byte)(sample >> 24);
+				
+				output.AddRange(chunk);
+				
+				length -= 4;
+			}
+
+			if (length != 0)
+			{
+				sample = this.Next();
+
+				for (int i = 0; i < length; i++)
+				{
+					output.Add((byte)sample);
+					sample >>= 8;
+				}
+			}
+
+			return output.ToArray();
+#endif
 		}
 
 		/// <inheritdoc/>
