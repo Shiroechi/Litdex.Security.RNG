b1611c7040d131ffea4bb6d77694567fcff53b68
Shiroechi <29156554+Shiroechi@users.noreply.github.com>
2021-08-05 16:18:06 +0700

    Add pcg variant
    the variant based on cpp source code from https://github.com/imneme/pcg-cpp/blob/master/include/pcg_random.hpp

---
+++ Source/Security/RNG/PRNG/PcgRxsMXs64.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Litdex.Security.RNG.PRNG
+{
+	/// <summary>
+	///		A Permuted Congruential Generator (PCG) that is composed of a 64-bit 
+	///		Linear Congruential Generator(LCG) combined with the RXS-M-XS (random xorshift; multiply; xorshift) output
+	///		transformation to create 64-bit output.
+	/// </summary>
+	/// <remarks>
+	///		https://www.pcg-random.org/
+	/// </remarks>
+	public class PcgRxsMXs64 : Random64
+	{
+		#region Member
+
+		protected ulong _Seed; //state in original code
+		protected ulong _Increment;
+		protected const ulong _Multiplier = 6364136223846793005;
+
+		#endregion Member
+
+		#region Constructor & Destructor
+
+		/// <summary>
+		///		Create an instance of <see cref="PcgRxsMXs64"/> object.
+		/// </summary>
+		/// <param name="seed">
+		///		RNG seed.
+		///	</param>
+		/// <param name="increment">
+		///		Increment step.
+		///	</param>
+		public PcgRxsMXs64(ulong seed = 0, ulong increment = 0)
+		{
+			this.SetSeed(seed, increment);
+		}
+
+		/// <summary>
+		///		Destructor.
+		/// </summary>
+		~PcgRxsMXs64()
+		{
+			this._Seed = 0;
+			this._Increment = 0;
+		}
+
+		#endregion Constructor & Destructor
+
+		#region Protected Method
+
+		/// <inheritdoc/>
+		protected override ulong Next()
+		{
+			var oldseed = this._Seed;
+			this._Seed = (oldseed * _Multiplier) + (this._Increment | 1);
+			ulong word = ((oldseed >> ((int)(oldseed >> 59) + 5)) ^ oldseed) * 12605985483714917081;
+			return (word >> 43) ^ word;
+		}
+
+		#endregion Protected Method
+
+		#region Public Method
+
+		/// <inheritdoc/>
+		public override string AlgorithmName()
+		{
+			return "PCG RXS-M-XS 64";
+		}
+
+		/// <inheritdoc/>
+		public override void Reseed()
+		{
+			ulong seed, increment;
+
+			using (var rng = new RNGCryptoServiceProvider())
+			{
+				var bytes = new byte[16];
+				rng.GetBytes(bytes);
+				seed = BitConverter.ToUInt64(bytes, 0);
+				increment = BitConverter.ToUInt64(bytes, 8);
+			}
+
+			this.SetSeed(seed, increment);
+		}
+
+		/// <summary>
+		///		Set <see cref="RNG"/> seed manually.
+		/// </summary>
+		/// <param name="seed">
+		///		RNG seed.
+		/// </param>
+		/// <param name="increment">
+		///		Increment step.
+		/// </param>
+		public void SetSeed(ulong seed, ulong increment)
+		{
+			this._Seed = (seed + increment) * _Multiplier + increment;
+			this._Increment = increment;
+		}
+
+		#endregion Public Method
+	}
+}
